b2432d7ad2bf821157de0df1ffadfd2f5b8e8194
Marco Alducin <sokram_r5_1992@hotmail.com>
2023-04-04 14:03:28 -0600

    Clase diferentes tipos de grid

---
+++ src/Components/App/index.js
@@ -0,0 +1,24 @@
+import { Grid } from '../Grid';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1 className="
+        bg-yellow-500 
+        text-red-600"
+      >
+        Hello world!
+      </h1>
+      <input className="
+        border-2 
+        border-blue-500 
+        placeholder-green-500" 
+      type='text' placeholder='Lorem ipsum' />
+      <Grid />
+    </React.Fragment>
+  );
+}
+
+export default App;
